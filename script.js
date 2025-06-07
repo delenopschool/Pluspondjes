a75d3473d869bc316d3fe1104dc31c4e2631698e
delenopschool <delenopschool@outlook.com>
2025-06-07 14:08:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Website geladen!");
 });
+
+function toggleMenu() {
+    document.getElementById("nav-links").classList.toggle("show");
+}
